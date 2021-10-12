1fd263dbd62e7485ee082ecf82e5a6344f62b247
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-12 11:00:58 +0000

    Added      Solutions/Task Solutions/TaskOne.cs

---
+++ Solutions/Task Solutions/TaskOne.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solutions.Task_Solutions
+{
+    public class TaskOne
+    {
+    }
+}
