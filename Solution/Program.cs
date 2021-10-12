e202191cddf5665bbe7bf3af6b855a40629dd19f
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-12 11:28:39 +0000

    Modified   Solution/Program.cs

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Solutions.Task_Solutions;
+using System;
 
 namespace Solution
 {
     class Program
     {
+        TaskOne taskOne = new TaskOne();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(taskOne.SumDigits(2,0,1,1));
         }
     }
 }
