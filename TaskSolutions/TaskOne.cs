5d8dc7cd6f096db41a5a4078f31ed82ae29748e0
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-12 12:24:42 +0000

    Added      TaskSolutions/TaskOne.cs Modified   Solution/.vs/Solution/v16/.suo

---
+++ TaskSolutions/TaskOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSolutions
+{
+    public class TaskOne
+    {
+        public void ReverseOrder(int a, int b, int c, int d)
+        {
+            List<int> check = new List<int>();
+
+            check.Add(a);
+            check.Add(b);
+            check.Add(c);
+            check.Add(d);
+
+            check.Reverse();
+        }
+    }
+}
