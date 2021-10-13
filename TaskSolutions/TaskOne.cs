f92684f0e1840bdf23b3896120419610211ab799
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-13 10:22:45 +0000

    Added      TaskSolutions/TaskOne.cs Added      TestTaskSolutions/TestTaskSolutions.csproj Added      TestTaskSolutions/UnitTest1.cs Added      TestTaskSolutions/obj/Debug/net5.0/.NETCoreApp,Version=v5.0.AssemblyAttributes.cs Added      TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.AssemblyInfo.cs Added      TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.AssemblyInfoInputs.cache Added      TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.GeneratedMSBuildEditorConfig.editorconfig Modified   TaskSolutions/.vs/TaskSolutions/v16/.suo Modified   TaskSolutions/TaskSolutions.sln

---
+++ TaskSolutions/TaskOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSolutions
+{
+    public class TaskOne
+    {
+        public int SumDigits(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+    }
+}
