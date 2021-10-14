b4ede4b8c65bc7c98aebbd12b1d1f44dc85c9ac5
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-14 09:36:49 +0000

    Modified   TaskSolutions/.vs/TaskSolutions/v16/.suo Modified   TestTaskSolutions/TaskOneTest.cs

--- TestTaskSolutions/TaskOneTest.cs
+++ TestTaskSolutions/TaskOneTest.cs
@@ -1,25 +1,23 @@
 using TaskSolutions;
-//using Xunit;
+using Xunit;
 
 namespace TestTaskSolutions
 {
     public class TaskOneTest
     {
-        //[Fact]
+        [Fact]
         public void TestingSumDigits()
         {
             //Arrange
-            //TaskOne taskOne = new TaskOne();
-            //taskOne.a = 2;
-            //taskOne.b = 0;
-            //taskOne.c = 1;
-            //taskOne.d = 1;
+
+            TaskOne taskOne = new TaskOne(2,0,1,1);
 
             //Assert
-            //expected = taskOne.SumDigits();
+            
 
             //Append
-            //
+
+
         }
     }
 }
