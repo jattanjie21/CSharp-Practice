04ab695069d64b7634a8a22d6a391ab8f115ccd9
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-14 18:40:40 +0000

    Added      TestTaskSolutions/TaskFourTest.cs Modified   TASKS.docx Modified   TaskSolutions/.vs/TaskSolutions/v16/.suo Modified   TaskSolutions/.vs/TaskSolutions/v16/TestStore/0/000.testlog Modified   TestTaskSolutions/bin/Debug/net5.0/TaskSolutions.dll Modified   TestTaskSolutions/bin/Debug/net5.0/TaskSolutions.pdb Modified   TestTaskSolutions/bin/Debug/net5.0/TestTaskSolutions.dll Modified   TestTaskSolutions/bin/Debug/net5.0/TestTaskSolutions.pdb Modified   TestTaskSolutions/bin/Debug/net5.0/ref/TestTaskSolutions.dll Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.csproj.CoreCompileInputs.cache Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.dll Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.pdb Modified   TestTaskSolutions/obj/Debug/net5.0/ref/TestTaskSolutions.dll

---
+++ TestTaskSolutions/TaskFourTest.cs
@@ -0,0 +1,23 @@
+﻿
+using TaskSolutions;
+using Xunit;
+
+namespace TestTaskSolutions
+{
+    public class TaskFourTest
+    {
+        [Fact]
+        public void TestCheckSumInput()
+        {
+            //Arrange
+            TaskFour taskFour = new();
+
+            //Act
+            bool expected = true;
+            bool actual = taskFour.CheckSumInput(30, 0);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
