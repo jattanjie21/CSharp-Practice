d15929ffbbd0cc8c2872454ad989510a8e2f6c1f
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-14 18:14:42 +0000

    Added      TestTaskSolutions/TaskThreeTest.cs Modified   TaskSolutions/.vs/TaskSolutions/v16/.suo Modified   TaskSolutions/.vs/TaskSolutions/v16/TestStore/0/000.testlog Modified   TaskSolutions/TaskThree.cs Modified   TaskSolutions/bin/Debug/net5.0/TaskSolutions.dll Modified   TaskSolutions/bin/Debug/net5.0/TaskSolutions.pdb Modified   TaskSolutions/obj/Debug/net5.0/TaskSolutions.dll Modified   TaskSolutions/obj/Debug/net5.0/TaskSolutions.pdb Modified   TestTaskSolutions/bin/Debug/net5.0/TaskSolutions.dll Modified   TestTaskSolutions/bin/Debug/net5.0/TaskSolutions.pdb Modified   TestTaskSolutions/bin/Debug/net5.0/TestTaskSolutions.dll Modified   TestTaskSolutions/bin/Debug/net5.0/TestTaskSolutions.pdb Modified   TestTaskSolutions/bin/Debug/net5.0/ref/TestTaskSolutions.dll Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.csproj.CoreCompileInputs.cache Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.dll Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.pdb Modified   TestTaskSolutions/obj/Debug/net5.0/ref/TestTaskSolutions.dll

---
+++ TestTaskSolutions/TaskThreeTest.cs
@@ -0,0 +1,24 @@
+﻿
+using TaskSolutions;
+using Xunit;
+
+namespace TestTaskSolutions
+{
+    public class TaskThreeTest
+    {
+        [Fact]
+        public void TestComputeSumCheckTwoDifferentNumbers()
+        {
+            //Arrange
+            TaskThree taskThree = new();
+
+            //Act
+            int expected = 5;
+            int actual = taskThree.ComputeSum(3, 2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
