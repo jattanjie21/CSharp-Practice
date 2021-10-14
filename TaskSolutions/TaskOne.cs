caaa3b31870bdb67895995698ffea33620773af7
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-14 11:04:12 +0000

    Modified   ConsoleTesting/Program.cs Modified   ConsoleTesting/bin/Debug/net5.0/ConsoleTesting.dll Modified   ConsoleTesting/bin/Debug/net5.0/ConsoleTesting.pdb Modified   ConsoleTesting/bin/Debug/net5.0/TaskSolutions.dll Modified   ConsoleTesting/bin/Debug/net5.0/TaskSolutions.pdb Modified   ConsoleTesting/obj/Debug/net5.0/ConsoleTesting.csproj.AssemblyReference.cache Modified   ConsoleTesting/obj/Debug/net5.0/ConsoleTesting.dll Modified   ConsoleTesting/obj/Debug/net5.0/ConsoleTesting.pdb Modified   TaskSolutions/.vs/TaskSolutions/v16/.suo Modified   TaskSolutions/.vs/TaskSolutions/v16/TestStore/0/000.testlog Modified   TaskSolutions/TaskOne.cs Modified   TaskSolutions/bin/Debug/net5.0/TaskSolutions.dll Modified   TaskSolutions/bin/Debug/net5.0/TaskSolutions.pdb Modified   TaskSolutions/bin/Debug/net5.0/ref/TaskSolutions.dll Modified   TaskSolutions/obj/Debug/net5.0/TaskSolutions.dll Modified   TaskSolutions/obj/Debug/net5.0/TaskSolutions.pdb Modified   TaskSolutions/obj/Debug/net5.0/ref/TaskSolutions.dll

--- TaskSolutions/TaskOne.cs
+++ TaskSolutions/TaskOne.cs
@@ -4,11 +4,16 @@ namespace TaskSolutions
 {
     public class TaskOne
     {
+        
         public int a { get; set; }
         public int b { get; set; }
         public int c { get; set; }
         public int d { get; set; }
         public List<int> collection { get; set; }
+        public int first { get; private set; }
+        public int second { get; private set; }
+        public int third { get; private set; }
+        public int last { get; private set; }
 
         public TaskOne(int a, int b, int c, int d)
         {
@@ -22,6 +27,11 @@ namespace TaskSolutions
             collection.Add(b);
             collection.Add(c);
             collection.Add(d);
+
+            first = collection[0];
+            second = collection[1];
+            third = collection[2];
+            last = collection[3];
         }
         public int SumDigits()
         {
@@ -35,28 +45,24 @@ namespace TaskSolutions
             return collection;
         }
 
-        public List<string> SwitchLastDigitToFirst()
-        {       
-            string last = collection[3].ToString();
-            string first = collection[1].ToString();
-            string second = collection[2].ToString();
-            string third = collection[0].ToString();
-
-            List<string> switchedDigits = new List<string> { last, first, second, third };
-
-            return switchedDigits;
-        }
-
-        public List<string> SwitchThirdDigitToSecond()
+        public List<int> SwitchLastDigitToFirst()
         {
-            string first = collection[0].ToString();
-            string third = collection[2].ToString();
-            string second = collection[1].ToString();
-            string last = collection[3].ToString();
-
-            List<string> switchedDigits = new List<string> { first, third, second, last };
+            
+            List<int> switchedDigits = new() { last, second, third, first };
 
             return switchedDigits;
         }
+
+        //public List<string> SwitchThirdDigitToSecond()
+        //{
+        //    string first = collection[0].ToString();
+        //    string third = collection[2].ToString();
+        //    string second = collection[1].ToString();
+        //    string last = collection[3].ToString();
+
+        //    List<string> switchedDigits = new List<string> { first, third, second, last };
+
+        //    return switchedDigits;
+        //}
     }
 }
