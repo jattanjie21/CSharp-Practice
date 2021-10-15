9c475ecfa28a05e9b591ce10eadd4b4d8789bdd1
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-15 11:06:26 +0000

    Added      TestTaskSolutions/TaskFiveTest.cs Modified   TaskSolutions/.vs/TaskSolutions/DesignTimeBuild/.dtbcache.v2 Modified   TaskSolutions/.vs/TaskSolutions/v16/.suo Modified   TaskSolutions/.vs/TaskSolutions/v16/TestStore/0/001.testlog Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.csproj.AssemblyReference.cache Modified   TestTaskSolutions/obj/Debug/net5.0/TestTaskSolutions.csproj.CoreCompileInputs.cache

---
+++ TestTaskSolutions/TaskFiveTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TaskSolutions;
+
+namespace TestTaskSolutions
+{
+    public class TaskFiveTest
+    {
+        [Fact]
+        public void TestPrintSomeText()
+        {
+            //Arrange
+            TaskFive taskFive = new();
+
+            //Act
+            string expected = "if else"
+            string actual = taskFive.PrintSomeText("if else");
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
