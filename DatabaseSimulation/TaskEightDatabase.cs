9057eae838daea72f57f96911c5bfb328bf96b20
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-25 09:56:53 +0000

    Modified   DatabaseSimulation/TaskEightDatabase.cs

--- DatabaseSimulation/TaskEightDatabase.cs
+++ DatabaseSimulation/TaskEightDatabase.cs
@@ -8,6 +8,8 @@ namespace DatabaseSimulation
 {
     public class TaskEightDatabase
     {
-
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
     }
 }
