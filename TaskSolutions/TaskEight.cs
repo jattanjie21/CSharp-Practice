723de482f48adadaf0241d677c85cbeb01a8d2ca
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-25 10:06:04 +0000

    Modified   TaskSolutions/TaskEight.cs

--- TaskSolutions/TaskEight.cs
+++ TaskSolutions/TaskEight.cs
@@ -11,6 +11,43 @@ namespace TaskSolutions
     {
         List<TaskEightDatabase> databaseObject = new List<TaskEightDatabase>();
 
+        public TaskEight()
+        {
+                
+        }
 
+        public int AddNewBook()
+        {
+            return 1;
+        }
+
+        public List<string> DisplayBookDetails()
+        {
+            return null;
+        }
+
+        public string SearchBookByTitle(string title)
+        {
+            return "title";
+        }
+
+        public string DeleteBookByID(int id)
+        {
+            //query id here
+            if (id!=0)            
+                return "found";
+            
+            else
+            {
+                return "book not found or available";
+            }
+        }
+
+        //options method here
+        //to allow users to 
+        //choose options
+        //preferably a switch 
+        //statement
     }
+
 }
