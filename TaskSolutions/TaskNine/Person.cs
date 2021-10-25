2fd3af809e4f4865608e2aefc367c7b7e3021589
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-25 11:45:04 +0000

    Modified   .vs/slnx.sqlite Modified   TaskSolutions/TaskNine/Person.cs

--- TaskSolutions/TaskNine/Person.cs
+++ TaskSolutions/TaskNine/Person.cs
@@ -3,5 +3,12 @@ namespace TaskSolutions.TaskNine
 {
     public class Person
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
     }
 }
