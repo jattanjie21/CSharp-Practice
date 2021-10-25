8bcd527c156d215282d045e22b903e086638a751
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-25 09:58:49 +0000

    Modified   TaskSolutions/TaskEight.cs

--- TaskSolutions/TaskEight.cs
+++ TaskSolutions/TaskEight.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseSimulation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace TaskSolutions
 {
     public class TaskEight
     {
-        
+        List<TaskEightDatabase> databaseObject = new List<TaskEightDatabase>();
+
+
     }
 }
