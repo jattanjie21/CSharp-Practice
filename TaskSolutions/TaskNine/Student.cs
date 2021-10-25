e19453ee28f49303a5176e1a7658a290412e64cd
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-25 11:55:19 +0000

    Modified   .vs/slnx.sqlite Modified   TaskSolutions/.vs/TaskSolutions/v16/.suo Missing    TaskSolutions/TaskNine.cs Modified   TaskSolutions/TaskNine/Student.cs Modified   TaskSolutions/TaskNine/Teacher.cs

--- TaskSolutions/TaskNine/Student.cs
+++ TaskSolutions/TaskNine/Student.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace TaskSolutions.TaskNine
 {
-    public class Student
+    public class Student : Person
     {
         public Student()
         {
