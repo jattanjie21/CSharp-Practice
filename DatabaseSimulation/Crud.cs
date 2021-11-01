0f147f9467c452c8110e8e2c5ecd2124d7fc8421
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-11-01 11:07:52 +0000

    Modified   DatabaseSimulation/Crud.cs

--- DatabaseSimulation/Crud.cs
+++ DatabaseSimulation/Crud.cs
@@ -3,6 +3,11 @@ namespace DatabaseSimulation
 {
     public class Crud
     {
+        public int ID { get; set; }
+        public string UserName { get; set;}
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Contact { get; set; }
 
     }
 }
